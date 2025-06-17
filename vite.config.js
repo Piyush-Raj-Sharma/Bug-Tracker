e086d2a0fa6d6b1e69930266bb1bcacc195d6f27
piyush-raj-sharma <rpiyush1357@gmail.com>
2025-06-17 07:24:57 +0530

    ...

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(), react()],
+  server: {
+    // This ensures refreshing routes like /dashboard doesn't throw 404
+    historyApiFallback: true
+  }
 })
