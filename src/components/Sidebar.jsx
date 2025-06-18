bbc51f7865e519ba170b674614b0c371544966a5
Piyush Raj <112124716+Piyush-Raj-Sharma@users.noreply.github.com>
2025-06-18 10:27:40 +0530

    Update Sidebar.jsx

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -46,7 +46,7 @@ const Sidebar = () => {
         {!collapsed ? (
           <>
             <h1 className="text-2xl font-bold transition-opacity duration-300">
-              FealtyX
+              Bug Tracker
             </h1>
             <button onClick={toggleSidebar}>
               <ChevronLeft size={24} />
